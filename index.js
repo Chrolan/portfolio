8f99cdc015ccc013ff941ad21ea3cc6fa5be3b55
Jordi Kleriga <jordikleriga@gmail.com>
2019-02-06 22:48:01 -0600

    added css to navbar`

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 //Initial configuration
+
+function navBarFunction() {
+    var x = document.getElementById("nav-bar");
+    if (x.className === "nav-bar") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-bar";
+    }
+}
+
+$('.nav-bar > a').on('click', function(event){
+    // First we take away the background color (selection) from the <li>
+    $('.selectedNavElement').removeClass('selectedNavElement');
+
+    let $currentElement = $(this);
+
+    // We update the just clicked <li> with the background color
+    $currentElement.addClass('selectedNavElement');
+
+    $('.js-error-message-box').empty()
+});
+
