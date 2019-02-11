6069073adc9c1219863b0d25a399bbf163e83114
Chrolan <jordikleriga@gmail.com>
2019-02-11 13:46:54 -0600

    added placement picture, UI additions

--- index.js
+++ index.js
@@ -17,7 +17,54 @@ $('.nav-bar-items > a').on('click', function(event){
 
     // We update the just clicked <li> with the background color
     $currentElement.addClass('selectedNavElement');
-
-    $('.js-error-message-box').empty()
 });
 
+//quick easy fix for navigating pages
+$('#bio-button').on('click', function (event) {
+
+    $('#bio').prop('hidden', false);
+
+    $('#project-1').prop('hidden', true);
+
+    $('#project-2').prop('hidden', true);
+
+    $('#project-3').prop('hidden', true);
+
+    console.log('test')
+});
+
+$('#project-1-button').on('click', function (event) {
+
+    $('#bio').prop('hidden', true);
+
+    $('#project-1').prop('hidden', false);
+
+    $('#project-2').prop('hidden', true);
+
+    $('#project-3').prop('hidden', true);
+
+});
+
+$('#project-2-button').on('click', function (event) {
+
+    $('#bio').prop('hidden', true);
+
+    $('#project-1').prop('hidden', true);
+
+    $('#project-2').prop('hidden', false);
+
+    $('#project-3').prop('hidden', true);
+
+});
+
+$('#project-3-button').on('click', function (event) {
+
+    $('#bio').prop('hidden', true);
+
+    $('#project-1').prop('hidden', true);
+
+    $('#project-2').prop('hidden', true);
+
+    $('#project-3').prop('hidden', false);
+
+});
